48f8b22977f80bf73a54ca4dbb6c9df9885b5896
Marius Küng <apps@mariuskueng.ch>
2014-03-16 13:25:26 +0100

    parse search results with mechanize

--- library_wishlist/parser.py
+++ library_wishlist/parser.py
@@ -2,6 +2,9 @@ import re
 import urllib2
 import urllib
 from bs4 import BeautifulSoup
+import mechanize
+
+base_URL = 'http://stadtbibliothekbasel.ch:8080'
 
 libraries = [
     'Zentrum',
@@ -11,13 +14,35 @@ libraries = [
     'Basel West',
 ]
 
+results = []
+
 def get_query_response(value):
     value = re.compile('\W+').sub(' ', value).strip()
     query = {'': '"%s"' % value}
     value = urllib.urlencode(query)
-    url = 'http://stadtbibliothekbasel.ch:8080/InfoGuideClient.sisis/start.do?Login=opextern&Language=de&SearchType=2&Query=-1%s' % value
-    response = urllib2.urlopen(url).read()
-    return BeautifulSoup(response)
+    url = '%s/InfoGuideClient.sisis/start.do?Login=opextern&Language=de&SearchType=2&Query=-1%s' % (base_URL, value)
+    br = mechanize.Browser()
+    response = br.open(url)
+    # response = urllib2.urlopen(url).read()
+    # print br.read()
+
+
+    soup = BeautifulSoup(response.read())
+    for r in soup.select('.data .t1'):
+        r = br.open(base_URL + soup.select('.data .t1')[0]['href'])
+        results.append(r.read())
+
+    for r in results:
+        soup = BeautifulSoup(r)
+        setItem(soup)
+    return
+
+    return response
+
+def getMultipleSearchResults(results):
+    pass
+
+def setItem(query):
 
 
 def getLibrary(branch):
@@ -27,8 +52,12 @@ def getLibrary(branch):
 
 
 def search_catalog(value):
-    soup = get_query_response(value)
-
+    query = get_query_response(value)
+    return
+    soup = BeautifulSoup(query.read())
+    # print soup
+    # print soup.search('#hitlist')
+    # return
     if (soup.select('#hitlist')): # if an artists list gets returned by query
         # TODO: return hitlist as recommandations to select
         return
@@ -81,6 +110,39 @@ def search_catalog(value):
 
     return item
 
-search_catalog('Black Keys Brothers')
+# Browser
+# value = 'Arbeit und Struktur'
+# value = re.compile('\W+').sub(' ', value).strip()
+# query = {'': '"%s"' % value}
+# value = urllib.urlencode(query)
+# url = '%s/InfoGuideClient.sisis/start.do?Login=opextern&Language=de&SearchType=2&Query=-1%s' % (base_URL, value)
+# br = mechanize.Browser()
+# print url
+# # Open some site, let's pick a random one, the first that pops in mind:
+# r = br.open(url)
+# html = r.read()
+# print html
+# # print html
+# soup = BeautifulSoup(html)
+
+# results = []
+# for r in soup.select('.data .t1'):
+#     results.append({
+#         'name': soup.select('.data .t1')[0].string.strip(),
+#         'url': base_URL + soup.select('.data .t1')[0]['href']
+#     })
+#     r = br.open(results['url'])
+#     print r.read()
+
+# # print results
+# print soup.select('p')
+
+# Show the source
+# print html
+# or
+# print br.response().read()
+
+
+search_catalog('Arbeit und Struktur')
 # print search_catalog('Black Keys Brothers')
 # print search_catalog('White lies big tv')
