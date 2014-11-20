33db60230b8546e5babb0fc9a4bee9215e29bd3a
Marius Küng <apps@mariuskueng.ch>
2014-11-20 13:34:34 +0100

    Added new tests for single search result items

--- library_wishlist/tests.py
+++ library_wishlist/tests.py
@@ -2,32 +2,79 @@ from django.test import TestCase
 from django.utils.timezone import now
 from parser import search_catalog
 from models import Item, Copy, Branch
+from django.db.models.base import ObjectDoesNotExist
 
 class ItemTestCase(TestCase):
+    testItemSingle = None
+    testItemMultiple = None
+    testBranch = None
+    search_item = None
+
     def setUp(self):
-        Item.objects.create(text="Bombay Bicycle Club")
+        self.testItemSingle = Item.objects.create(text="Bombay Bicycle Club")
+        self.testItemMultiple = Item.objects.create(text="Coldplay")
+        self.testBranch = Branch.objects.create(name="Kirschgarten")
+
+        # saving the searchitem to reduce latency
+        self.search_item = search_catalog(self.testItemSingle.text)
+        self.testItemSingle.createCopies(self.search_item)
+
+        # define fixtures states
+        # - amount of copies
+        # - copies statuses
 
     def test_item_has_text(self):
-        test_item = Item.objects.get(text="Bombay Bicycle Club")
-        self.assertEqual(test_item.text, "Bombay Bicycle Club")
-
-    def test_item_search_catalog_with_invalid_text(self):
-        text="Bombays Bicycle Clubb" # with typos
-        search_item = search_catalog(text)
-
-        self.assertEqual(search_item, [])
-
-    # saving the searchitem to reduce latency
-    search_item = None
+        self.assertEqual(self.testItemSingle.text, "Bombay Bicycle Club")
 
     def test_item_search_catalog_with_valid_item_text(self):
         """Search the catalog for this item text"""
-        test_item = Item.objects.get(text="Bombay Bicycle Club")
-        search_item = search_catalog(test_item.text)
+        self.testItemSingle = Item.objects.get(text="Bombay Bicycle Club")
 
-        self.assertEqual(search_item['name'], 'So long, see you tomorrow')
+        self.assertEqual(self.search_item['name'], 'So long, see you tomorrow')
+
+    def test_item_search_catalog_with_invalid_text(self):
+        text="Bombays Bicycle Clubb" # with typos
+        self.search_item = search_catalog(text)
+
+        self.assertEqual(self.search_item, [])
 
     def test_item_has_valid_date(self):
+        """Create new item and test if it's date is equal to now"""
+        right_now = now()
+        self.testItemSingle.created = right_now
+
+        self.assertEqual(self.testItemSingle.created, right_now)
+
+    def test_delete_item(self):
+        testItemSingle = Item.objects.create(text="Bombay Bicycle Club")
+        testItemSingle.delete()
+        try:
+            Item.objects.get(id=testItemSingle.id)
+        except ObjectDoesNotExist, e:
+            self.assertRaises(e)
+
+    def test_complete_item(self):
+        self.assertEqual(self.testItemSingle.completed, False)
+        self.testItemSingle.completed = True
+        self.assertEqual(self.testItemSingle.completed, True)
+
+    def test_item_create_copies(self):
+        self.assertEqual(self.testItemSingle.copies.count(), 1)
+        self.assertEqual(self.testItemSingle.copies.all()[0].item.id, self.testItemSingle.id)
+
+    def test_item_set_item_status(self):
+        self.testItemSingle.setItemStatus()
+        # should be True if Item is available
+        self.assertEqual(self.testItemSingle.status, True)
+
+    def test_copies_with_item(self):
+        test_copies = Copy.objects.filter(item=self.testItemSingle)
+        self.assertEqual(test_copies.count(), self.testItemSingle.copies.count())
+
+    def test_item_with_multiple_search_results(self):
+        pass
+
+    def test_item_search_index(self):
         pass
 
 
@@ -39,6 +86,6 @@ class ItemTestCase(TestCase):
         - test Copy model with item
         - test item lists
         - test valid searchIndex with new catalog search
-        - test management command with update copies
         - test views...
+        - test User
     '''
